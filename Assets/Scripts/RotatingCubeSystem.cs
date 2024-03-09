725b3f4c847a284a3a888ad402ec62f66a311ddc
Cenei <86491216+Cennei@users.noreply.github.com>
2024-03-09 08:42:58 +0300

    Update RotatingCubeSystem.cs
    IJobEntity

--- Assets/Scripts/RotatingCubeSystem.cs
+++ Assets/Scripts/RotatingCubeSystem.cs
@@ -1,5 +1,7 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst;
 using Unity.Entities;
 using Unity.Transforms;
 using UnityEngine;
@@ -14,15 +16,35 @@ public partial struct RotatingCubeSystem : ISystem
 
     }
 
+    [BurstCompile]
+
     public void OnUpdate(ref SystemState state) 
     {
-        
+        /*
         foreach (var ( localTransform, rotateSpeed) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotateSpeed>>()) //RefRW means Read and Write (RefRO for Read only)
         {
 
             localTransform.ValueRW = localTransform.ValueRO.RotateY(rotateSpeed.ValueRO.value * SystemAPI.Time.DeltaTime); 
 
         } 
+        */
+        RotatingCubeJob rotatingCubeJob = new RotatingCubeJob
+        {
+            deltaTime = SystemAPI.Time.DeltaTime
+        };
+        rotatingCubeJob.Schedule();
+    }
+
+    [BurstCompile]
+    public partial struct RotatingCubeJob : IJobEntity
+    {
+        public float deltaTime;
+
+        public void Execute(ref LocalTransform localTransform, in RotateSpeed rotateSpeed)
+        {
+        localTransform = localTransform.RotateY(rotateSpeed.value* deltaTime);
+
+        }
 
     }
 
